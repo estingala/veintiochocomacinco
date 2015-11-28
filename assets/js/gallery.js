5acd6c02bf70289f2bcf1bfff0b9ab568c3cca66
Num Mendez <num@jacoti.com>
2015-11-28 18:13:57 +0100

    wip

---
+++ assets/js/gallery.js
@@ -0,0 +1,13 @@
+/**
+ * Created by estin on 21/11/15.
+ */
+$(document).ready(function(){
+    $('.top-main-gallery').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 4000
+    });
+});
