3850032a79ec1ac191f3954cecd6fd0d7b87629f
Num Mendez <num@jacoti.com>
2016-02-22 00:02:16 +0100

    wip

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -68,3 +68,24 @@ $(document).ready(function(){
 //        }); // end window scroll
 //    });  // end section function
 //}); // close out script
+$(document).ready(function(){
+    // cache the window object
+    $window = $(window);
+
+    $('section[data-type="post-main-title"]').each(function(){
+        // declare the variable to affect the defined data-type
+        var $scroll = $(this);
+
+        $(window).scroll(function() {
+            // HTML5 proves useful for helping with creating JS functions!
+            // also, negative value because we're scrolling upwards
+            var yPos = ($window.scrollTop() / $scroll.data('speed'));
+
+            // background position
+            var coords = yPos + 'px';
+
+            // move the background
+            $scroll.css({ top: coords });
+        }); // end window scroll
+    });  // end section function
+}); // close out script
