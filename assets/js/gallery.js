97ac8869ffe01d6644a2ff424680bc35da9af6b2
Num Mendez <num@jacoti.com>
2016-02-21 22:35:22 +0100

    wip

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -13,55 +13,58 @@ $(document).ready(function(){
         autoplaySpeed: 4000
     });
 });
-
-window.addEventListener('scroll', function() {
-    //var l = $('*').length;
-    //for(var i = 0; i < l; i++) {
-    //    var elem = $('*:eq(' + i + ')');
-    //    if(elem.scrollTop() > 0) {
-    //        console.log(elem, elem.scrollTop());
-    //    }
-    //}
-
-
-
-    //console.log(frames.top.scrollY)
-    //console.log($('body').scrollTop())
-    if (window.scrollY > 100) {
-        //$("#img_logo_shoe").fadeOut('slow');
-        ////$("#img_logo_letters").show();
-        ////$("#img_logo_shoe").opacity = 0;
-        $("#img_logo_shoe").css({ opacity: 0.0 });
-        $('nav').removeClass('navbar-large');
-        $('nav').addClass('navbar-small');
-    } else {
-        $("#img_logo_shoe").css({ opacity: 1.0 });
-        //$("#img_logo_shoe").fadeIn("slow");
-        ////$("#img_logo_letters").hide();
-        //$("#img_logo_shoe").opacity = 1;
-        $('nav').addClass('navbar-large');
-        $('nav').removeClass('navbar-small');
-    }
-});
-
-$(document).ready(function(){
-    // cache the window object
-    $window = $(window);
-
-    $('section[data-type="gallery"]').each(function(){
-        // declare the variable to affect the defined data-type
-        var $scroll = $(this);
-
-        $(window).scroll(function() {
-            // HTML5 proves useful for helping with creating JS functions!
-            // also, negative value because we're scrolling upwards
-            var yPos = ($window.scrollTop() / $scroll.data('speed'));
-
-            // background position
-            var coords = yPos + 'px';
-
-            // move the background
-            $scroll.css({ top: coords });
-        }); // end window scroll
-    });  // end section function
-}); // close out script
+//
+//window.addEventListener('scroll', function() {
+//    //var l = $('*').length;
+//    //for(var i = 0; i < l; i++) {
+//    //    var elem = $('*:eq(' + i + ')');
+//    //    if(elem.scrollTop() > 0) {
+//    //        console.log(elem, elem.scrollTop());
+//    //    }
+//    //}
+//
+//
+//
+//    //console.log(frames.top.scrollY)
+//    //console.log($('body').scrollTop())
+//    if (window.scrollY > 0) {
+//        $("#grid").addClass('shadowed');
+//        //$("#img_logo_shoe").fadeOut('slow');
+//        ////$("#img_logo_letters").show();
+//        ////$("#img_logo_shoe").opacity = 0;
+//        //$("#img_logo_shoe").css({ opacity: 0.0 });
+//        //$('nav').removeClass('navbar-large');
+//        //$('nav').addClass('navbar-small');
+//    } else {
+//
+//        $("#grid").removeClass('shadowed');
+//        //$("#img_logo_shoe").css({ opacity: 1.0 });
+//        ////$("#img_logo_shoe").fadeIn("slow");
+//        //////$("#img_logo_letters").hide();
+//        ////$("#img_logo_shoe").opacity = 1;
+//        //$('nav').addClass('navbar-large');
+//        //$('nav').removeClass('navbar-small');
+//    }
+//});
+//
+//$(document).ready(function(){
+//    // cache the window object
+//    $window = $(window);
+//
+//    $('section[data-type="gallery"]').each(function(){
+//        // declare the variable to affect the defined data-type
+//        var $scroll = $(this);
+//
+//        $(window).scroll(function() {
+//            // HTML5 proves useful for helping with creating JS functions!
+//            // also, negative value because we're scrolling upwards
+//            var yPos = ($window.scrollTop() / $scroll.data('speed'));
+//
+//            // background position
+//            var coords = yPos + 'px';
+//
+//            // move the background
+//            $scroll.css({ top: coords });
+//        }); // end window scroll
+//    });  // end section function
+//}); // close out script
